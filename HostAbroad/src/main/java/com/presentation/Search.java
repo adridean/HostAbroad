249c8c043e1cd3a7b10ad7d153d71fdbe42f6d2e
evivar <42641415+evivar@users.noreply.github.com>
2019-03-18 18:52:27 +0100

    Update Search.java
    RadioButtonGroup instead of two combo boxes.
    Handling exceptions

--- HostAbroad/src/main/java/com/presentation/Search.java
+++ HostAbroad/src/main/java/com/presentation/Search.java
@@ -7,13 +7,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import com.business.User;
+import com.vaadin.server.UserError;
 import com.vaadin.shared.ui.window.WindowMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.RadioButtonGroup;
 import com.vaadin.ui.TextArea;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 public class Search extends Window {
@@ -27,88 +32,94 @@ public class Search extends Window {
 
 	private Button search;
 
-	private CheckBox cbHosts;
+	private RadioButtonGroup<String> rbGroup;
 
-	private CheckBox cbTravelers;
-	
 	private TextArea results;
 
 	public Search() {
-		layout = new GridLayout(2, 3);
-		layout.setMargin(true);
-		layout.setSpacing(true);
+		HorizontalLayout hL = new HorizontalLayout();
+		VerticalLayout vL = new VerticalLayout();
+		rbGroup = new RadioButtonGroup<String>();
+		rbGroup.setItems("Hosts", "Travelers");
 		search = new Button("Search");
 		search.addClickListener(new ClickListener() {
 
 			@Override
 			public void buttonClick(ClickEvent event) {
+				System.out.println(rbGroup.getValue());
 				results.clear();
-				if (cbHosts.getValue() == true && cbTravelers.getValue() == false) {
-					List<User> hosts = readAllHosts();
-					if(hosts.size() > 0) {
-						for(User u : hosts) {
-							results.setValue(results.getValue() + u.getNickName() + "\n");
+				search.setComponentError(null);
+				try {
+					if (rbGroup.getValue().equals("Hosts")) {
+						List<User> hosts = readAllHosts();
+						if (hosts.size() > 0) {
+							for (User u : hosts) {
+								results.setValue(results.getValue() + u.getNickName() + "\n");
+							}
+						} else {
+							results.setValue("There are no hosts");
+							Notification.show("There are no hosts");
 						}
-					}
-					else {
-						results.setValue("There are no hosts");
-					}
-				}
-				else if (cbHosts.getValue() == false && cbTravelers.getValue() == true){
-					List<User> travelers = readAllTravelers();
-					if(travelers.size() > 0) {
-						for(User u : travelers) {
-							results.setValue(results.getValue() + u.getNickName() + "\n");
+					} else if (rbGroup.getValue().equals("Travelers")) {
+						List<User> travelers = readAllTravelers();
+						if (travelers.size() > 0) {
+							for (User u : travelers) {
+								results.setValue(results.getValue() + u.getNickName() + "\n");
+							}
+						} else {
+							results.setValue("There are no travelers");
+							Notification.show("There are no travelers");
 						}
+					} else if (!rbGroup.isSelected("Hosts") || !rbGroup.isSelected("Travelers")) {
+						throw new Exception();
 					}
-					else {
-						results.setValue("There are no travelers");
-					}
-				}
-				else {
-					results.setValue("You have to choose between Hosts or Travelers to perform the search");
+				} catch (Exception e) {
+					search.setComponentError(
+							new UserError("You have to choose Hosts or Travelers to perform the search"));
 				}
 			}
 
 		});
-		cbHosts = new CheckBox("Hosts");
-		cbTravelers = new CheckBox("Travelers");
 		results = new TextArea();
 		results.setEnabled(false);
-		layout.addComponent(cbHosts, 0, 0);
-		layout.addComponent(cbTravelers, 0, 1);
-		layout.addComponent(search, 0, 2);
-		layout.addComponent(results, 1, 0, 1, 2);
-		this.setContent(layout);
+		results.setSizeFull();
+		vL.setSizeUndefined();
+		vL.addComponent(rbGroup);
+		vL.addComponent(search);
+		hL.setMargin(true);
+		hL.setSizeFull();
+		hL.addComponent(vL);
+		hL.addComponent(results);
+		this.setContent(hL);
 		this.setClosable(false);
 		this.setWindowMode(WindowMode.MAXIMIZED);
 		this.setResizable(false);
 	}
-	
-	public List<User> readAllHosts(){
+
+	public List<User> readAllHosts() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("HostAbroad");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        List<User> hosts = em.createNativeQuery("SELECT * FROM User WHERE Host = 1", User.class).getResultList();
-        
-        em.close( );
-        emf.close( );
-        
-        return hosts;
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<User> hosts = em.createNativeQuery("SELECT * FROM User WHERE Host = 1", User.class).getResultList();
+
+		em.close();
+		emf.close();
+
+		return hosts;
 	}
-	
-	public List<User> readAllTravelers(){
+
+	public List<User> readAllTravelers() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("HostAbroad");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        List<User> travelers = em.createNativeQuery("SELECT * FROM User WHERE Host = 0", User.class).getResultList();
-        
-        em.close( );
-        emf.close( );
-        
-        return travelers;
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<User> travelers = em.createNativeQuery("SELECT * FROM User WHERE Host = 0", User.class).getResultList();
+
+		em.close();
+		emf.close();
+
+		return travelers;
 	}
 
 }
